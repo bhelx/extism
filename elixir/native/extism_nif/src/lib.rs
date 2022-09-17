5a8898e2971b89bee133eaf33d6a36e22d6dd1a4
Benjamin Eckel <bhelx@simst.im>
2022-09-17 11:03:44 -0500

    Let the SDK handle the manifest parsing and validation

--- elixir/native/extism_nif/src/lib.rs
+++ elixir/native/extism_nif/src/lib.rs
@@ -1,7 +1,6 @@
-use rustler::{Error, Atom};
+use rustler::{Atom};
 use extism::{Plugin};
 use std::str;
-use serde_json;
 use std::path::Path;
 use std::str::FromStr;
 
@@ -17,54 +16,49 @@ mod atoms {
 // fn load(env: Env, _: Term) -> bool {
 //     true
 // }
-
- #[rustler::nif]
-fn plugin_new_with_manifest(manifest_payload: String, wasi: bool) -> Result<isize, Error> {
-    match serde_json::from_str(&manifest_payload) {
-        Ok(manifest) => {
-            match Plugin::new_with_manifest(&manifest, wasi) {
-                Err(_e) => Err(Error::Term(Box::new("Could not load plugin"))),
-                Ok(plugin) => Ok(plugin.as_isize())
-            }
-        },
-        Err(_e) => Err(Error::Term(Box::new("Could not parse manifest")))
+fn to_rustler_error(extism_error: extism::Error) -> rustler::Error {
+    match extism_error {
+        extism::Error::UnableToLoadPlugin => rustler::Error::Term(Box::new("Unable to load plugin for unknown reason")),
+        extism::Error::Message(msg) => rustler::Error::Term(Box::new(msg)),
+        extism::Error::Json(json_err) => rustler::Error::Term(Box::new(json_err.to_string()))
     }
 }
 
  #[rustler::nif]
-fn call_plugin(plugin_iz: isize, name: String, input: String) -> Result<String, Error> {
+fn plugin_new_with_manifest(manifest_payload: String, wasi: bool) -> Result<isize, rustler::Error> {
+    match Plugin::new(manifest_payload, wasi) {
+        Err(e) => Err(to_rustler_error(e)),
+        Ok(plugin) => Ok(plugin.as_isize())
+    }
+}
+
+ #[rustler::nif]
+fn call_plugin(plugin_iz: isize, name: String, input: String) -> Result<String, rustler::Error> {
     let plugin = Plugin(plugin_iz);
     match plugin.call(name, input) {
-        Err(_e) => Err(Error::Term(Box::new("Failed to call plugin"))),
+        Err(e) => Err(to_rustler_error(e)),
         Ok(result) => {
             match str::from_utf8(&result) {
                 Ok(output) => Ok(output.to_string()),
-                Err(_e) => Err(Error::Term(Box::new("Could not read output from plugin")))
+                Err(_e) => Err(rustler::Error::Term(Box::new("Could not read output from plugin")))
             }
         }
     }
 }
 
  #[rustler::nif]
-fn update_manifest(plugin_iz: isize, manifest_payload: String, wasi: bool) -> Result<bool, Error> {
+fn update_manifest(plugin_iz: isize, manifest_payload: String, wasi: bool) -> Result<bool, rustler::Error> {
     let plugin = &mut Plugin(plugin_iz);
-    match serde_json::from_str(&manifest_payload) {
-        Ok(manifest) => {
-            match plugin.update_manifest(&manifest, wasi) {
-                Err(_e) => Err(Error::Term(Box::new("Could not update manifest"))),
-                Ok(updated) => Ok(updated)
-            }
-        },
-        Err(_e) => Err(Error::Term(Box::new("Could not parse manifest")))
-    }
+    let result = plugin.update(manifest_payload, wasi);
+    Ok(result)
 }
 
 #[rustler::nif]
-fn set_log_file(plugin_iz: isize, filename: String, log_level: String) -> Result<Atom, Error> {
+fn set_log_file(plugin_iz: isize, filename: String, log_level: String) -> Result<Atom, rustler::Error> {
     let plugin = &Plugin(plugin_iz);
     let path = Path::new(&filename);
     match log::LevelFilter::from_str(&log_level) {
-        Err(_e) =>  Err(Error::Term(Box::new(format!("Invalid log level {}", log_level)))),
+        Err(_e) => Err(rustler::Error::Term(Box::new(format!("{} not a valid log level", log_level)))),
         Ok(level) => {
             plugin.set_log_file(path, Some(level));
             Ok(atoms::ok())
@@ -73,7 +67,7 @@ fn set_log_file(plugin_iz: isize, filename: String, log_level: String) -> Result
 }
 
 #[rustler::nif]
-fn plugin_has_function(plugin_iz: isize, function_name: String) -> Result<bool, Error> {
+fn plugin_has_function(plugin_iz: isize, function_name: String) -> Result<bool, rustler::Error> {
     let plugin = &Plugin(plugin_iz);
     Ok(plugin.has_function(function_name))
 }
