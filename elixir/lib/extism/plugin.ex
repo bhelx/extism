b50b575ed44822c4b2294950ce8856868832b2ed
Benjamin Eckel <bhelx@simst.im>
2022-09-17 11:28:31 -0500

    Change property name to plugin_index

--- elixir/lib/extism/plugin.ex
+++ elixir/lib/extism/plugin.ex
@@ -1,19 +1,12 @@
 defmodule Extism.Plugin do
   defstruct [
-    # The actual NIF Resource.
-    resource: nil,
-    # Normally the compiler will happily do stuff like inlining the
-    # resource in attributes. This will convert the resource into an
-    # empty binary with no warning. This will make that harder to
-    # accidentaly do.
-    # It also serves as a handy way to tell plugins apart.
-    reference: nil
+    # The actual NIF Resource. PluginIndex in this case
+    plugin_index: nil,
   ]
 
-  def wrap_resource(resource) do
+  def wrap_resource(plugin_index) do
     %__MODULE__{
-      resource: resource,
-      reference: make_ref()
+      plugin_index: plugin_index
     }
   end
 
@@ -26,7 +19,7 @@ defmodule Extism.Plugin do
   end
 
   def call(plugin, name, input) do
-    case Extism.Native.call_plugin(plugin.resource, name, input) do
+    case Extism.Native.call_plugin(plugin.plugin_index, name, input) do
       {:error, err} -> {:error, err}
       res -> {:ok, res}
     end
@@ -34,18 +27,18 @@ defmodule Extism.Plugin do
 
   def update(plugin, manifest, wasi) when is_map(manifest) do
     {:ok, manifest_payload} = JSON.encode(manifest)
-    case Extism.Native.update_manifest(plugin.resource, manifest_payload, wasi) do
+    case Extism.Native.update_manifest(plugin.plugin_index, manifest_payload, wasi) do
       {:error, err} -> {:error, err}
       res -> {:ok, res}
     end
   end
 
   def set_log_file(plugin, filename, log_level) do
-    Extism.Native.set_log_file(plugin.resource, filename, log_level)
+    Extism.Native.set_log_file(plugin.plugin_index, filename, log_level)
   end
 
   def has_function(plugin, function_name) do
-    Extism.Native.plugin_has_function(plugin.resource, function_name)
+    Extism.Native.plugin_has_function(plugin.plugin_index, function_name)
   end
 end
 
@@ -53,6 +46,6 @@ defimpl Inspect, for: Extim.Plugin do
   import Inspect.Algebra
 
   def inspect(dict, opts) do
-    concat(["#Extism.Plugin<", to_doc(dict.reference, opts), ">"])
+    concat(["#Extism.Plugin<", to_doc(dict.plugin_index, opts), ">"])
   end
 end
